a22661ff1c417b509a51be26e96aa9d1fff2b6c6
mtgezgin <mehmetturgutgezgin@gmail.com>
2022-01-06 10:48:56 +0300

    Shifted expense item by hardcoding to dynamic coding.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,10 +13,9 @@ const Expenses = props => {
     return (
         <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilterByYear={filterByYearHandler}/>
-            <ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date} />
-            <ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date} />
-            <ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date} />
-            <ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date} />
+        {items.map(item => (
+            <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date} />
+        ))}
         </Card>
     )
 }
